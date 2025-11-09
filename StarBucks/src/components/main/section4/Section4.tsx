2a8e856b10efffb125d765554d91d04a03266b88
vadim <vadimdrigula@gmail.com>
2025-11-09 22:26:50 +0200

    section4

--- StarBucks/src/components/main/section4/Section4.tsx
+++ StarBucks/src/components/main/section4/Section4.tsx
@@ -1,7 +1,12 @@
 import './section4.scss'
 function Section4() {
     return (
-        <div>Section4</div>
+        <section className="section4">
+            <div className="section4-container">
+                <p className="section4-container__subtitle">Only in 2021 we have made more than 100,000 orders for you, your loved ones, all of you, and in 2022 we are ready to destroy the market</p>
+                <p className="section4-container__title">Our New <span className="section4-container__title--span">Events</span></p>
+            </div>
+        </section>
     )
 }
 
